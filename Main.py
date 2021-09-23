010466a5f97a9309a4d3d3ef27281a27e6b97d9a
Morse <enmorse48@gmail.com>
2021-09-23 12:08:45 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,18 @@
+# Make a list that includes at least three people you
+# would like to invite to dinner.
+
+# Then use your list to print a message to each person,
+# inviting them to dinner.
+
+idol_guest_list = ["arnold schwarzenegger", "babe ruth",
+                   "sean connery"]
+
+message = f"Mr. {idol_guest_list[0].title()}, " \
+          f"Mr. {idol_guest_list[1].title()}, and " \
+          f"Mr. {idol_guest_list[2].title()}, " \
+          f"\nI would like to invite you to dinner because " \
+          f"you are idols of mine and I have admired " \
+          f"\nyou and looked up to you sense I was a " \
+          f"little boy."
+
+print(message)
